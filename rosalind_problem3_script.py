70049b0dcfa9c1f63c3b3025edd4bc1c95fe0f63
J3TT <jett25@email.arizona.edu>
2015-02-16 14:51:18 -0700

    Rosalind problem three script

---
+++ rosalind_problem3_script.py
@@ -0,0 +1,8 @@
+#!bin/user/python
+data_set = '94I8zP9OqfTcBhqtel4AQWnonxdi0VKT5IX3qzSalmoabmDa3IlKX8gpNWvfYQK5oqEcezj3m3LxVvaVdKulSWao2ts4lxKvjQLuJ0yraYhbycGY45BCofSS3hG8QWNuKPKXLafuIs1A2IObF5abK1ZJkJA7tarandusaNzLw9T3.'
+a = 38
+b = 42
+c = 156
+d = 163
+answer = (data_set[a:b+1]) + " " + (data_set[c:d+1])
+print (answer)
